107bb81b5a1cff805cd13b1bc4955489993e84c3
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-04-19 22:11:55 +0200

    Update database version

--- src/lib/interfaces/db.ts
+++ src/lib/interfaces/db.ts
@@ -6,7 +6,7 @@ export class Database extends Dexie {
 
     constructor() {
         super('Listr');
-        this.version(2).stores({
+        this.version(3).stores({
             users: '++id, name, displayName, image, banner, bio, nip05, lud16, about, zapService, lastFetched',
             lists: '++id, name, kind, createdAt, authorHexPubkey, publicItems*, privateItems*, npub, hexpubkey, lastFetched, [authorHexPubkey+name+kind]'
         });
