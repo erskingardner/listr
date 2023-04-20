8dc0aad93d9baa9f5aec9b6f1690514d4f21ee89
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-04-20 09:01:56 +0200

    Re-enable analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,9 @@
     import Footer from '$lib/components/Footer.svelte';
     import { onMount } from 'svelte';
     import { currentUser } from '$lib/stores/currentUser';
-    // import { dev } from '$app/environment';
-    // import { inject } from '@vercel/analytics';
-    // inject({ mode: dev ? 'development' : 'production' });
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });
 
     let savestore = false;
 
