1377589e93920bb393aa474f471d3dd12416eb0b
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-09 22:41:13 +0200

    Update link in footer to Primal

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,10 +5,7 @@
 <div class="mt-10 md:mt-20 border-t border-stone-800/20 dark:border-stone-100/20">
     <div class="mt-4 text-center">
         <p>
-            Made with 💜 & ⚡ by <a
-                href="https://snort.social/p/npub1zuuajd7u3sx8xu92yav9jwxpr839cs0kc3q6t56vd5u9q033xmhsk6c2uc"
-                >JeffG</a
-            >
+            Made with 💜 & ⚡ by <a href="https://primal.net/jeffg">JeffG</a>
         </p>
     </div>
 </div>
