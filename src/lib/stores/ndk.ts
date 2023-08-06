73479d9c92ea165bd01c0cd2f62ef6c138183d45
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-06 09:21:21 +0200

    Update relays

--- src/lib/stores/ndk.ts
+++ src/lib/stores/ndk.ts
@@ -3,12 +3,12 @@ import NDK from '@nostr-dev-kit/ndk';
 
 const ndk = new NDK({
     explicitRelayUrls: [
-        'ws://localhost:8080'
-        // 'wss://purplepag.es',
-        // 'wss://relay.nostr.band',
-        // 'wss://nos.lol',
-        // 'wss://relay.snort.social',
-        // 'wss://relay.damus.io'
+        // 'ws://localhost:8080'
+        'wss://purplepag.es',
+        'wss://relay.nostr.band',
+        'wss://nos.lol',
+        'wss://relay.snort.social',
+        'wss://relay.damus.io'
     ]
 });
 
