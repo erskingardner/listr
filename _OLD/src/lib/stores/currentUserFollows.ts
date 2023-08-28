c50a9d166091cef7154508e38baa27b9c72bf02b
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-28 22:17:35 +0200

    Remove OLD dir

--- _OLD/src/lib/stores/currentUserFollows.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-
-// Will be an array of npubs of the users that the current user follows
-export const currentUserFollows = writable<string[] | undefined>(undefined);
